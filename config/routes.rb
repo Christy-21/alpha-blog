cdb85eb68f49ef99ed6e6412c8aa4a06f2082773
Christy-21 <christy211202@gmail.com>
2023-06-08 11:57:11 +0530

    Pages added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    
+     root 'pages#home'
+     get 'about', to: 'pages#about'
 
   # Defines the root path route ("/")
   # root "articles#index"
